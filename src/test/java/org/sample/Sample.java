78e6702fe474205e9f3889aa030a6762c3516499
Nirmal021197 <nirmalpalanimudali@gmail.com>
2024-09-12 11:27:24 +0530

    Business logic created

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -2,19 +2,19 @@ package org.sample;
 
 public class Sample {
 public void id() {
-	
+System.out.println("001");	
 }
 public void role()
 {
-	
+System.out.println("Tester");	
 }
 public void experience()
 {
-	
+System.out.println("3yerars");	
 }
 public void team()
 {
-	
+	System.out.println("QA");
 }
 public static void main(String[] args) {
 	
