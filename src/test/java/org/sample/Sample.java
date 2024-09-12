6c1cb2affbe7460aef3e4cec86d3f541bc84d682
Nirmal021197 <nirmalpalanimudali@gmail.com>
2024-09-12 11:15:49 +0530

    added

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -1,5 +1,24 @@
 package org.sample;
 
 public class Sample {
-
+public void id() {
+	
+}
+public void role()
+{
+	
+}
+public void experience()
+{
+	
+}
+public void team()
+{
+	
+}
+public static void main(String[] args) {
+	
+	Sample s=new Sample();
+	s.id();
+}
 }
